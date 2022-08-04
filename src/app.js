41d3e4c9b411075dc6e9caf77b9359f361a783d1
LaurinLF <laura_lf20@hotmail.com>
2022-08-04 19:50:52 -0300

    actualizaciones delete button

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const methodOverride =  require('method-override');
 const port = process.env.PORT || 3000;
 
 
+app.use(methodOverride('_method')); 
+app.use(express.json());
 
 let publicPath = path.resolve(__dirname, "../public")
 app.use(express.static(publicPath))
@@ -25,6 +28,22 @@ app.use("/products", productRouter)
 
 
 
+
+
+
+
+app.use((req, res, next) => {
+    const error = new Error('Error 404 - No se encontró la pagina solicitada');
+    res.status(404).render('error', {
+        message: error.message,
+        path: `http://${req.hostname}:${port}${req.url}`,
+        status: 404,
+        error
+    })
+})
+
 app.listen(port, () => {
     console.log("Servidor corriendo en el puerto " + port);
 })
+
+
